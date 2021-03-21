bc3637699b3a1c93e965c73f40981c5db35ea9d9
filipe <filipe.bonfim@stoom.com.br>
2021-03-20 23:35:28 -0300

    adicionando product swagger

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,74 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - originalPrice
+ *         - promotionalPrice
+ *         - expiryDate
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the product
+ *         name:
+ *           type: string
+ *           description: The name of the product
+ *         originalPrice:
+ *           type: double
+ *           description: The original price
+ *         promotionalPrice:
+ *           type: double
+ *           description: Price with promotion
+ *         expiryDate:
+ *           type: date
+ *           description: The date that the product expiry
+ *       example:
+ *         id: d5fE_asz
+ *         name: Mamão
+ *         originalPrice: 60.22
+ *         promotionalPrice: 12.1
+ *         expiryDate: 2020-01-01
+ */
+
+/**
+* @swagger
+* tags:
+*   name: Products
+*   description: The products managing API
+*/
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Returns the list of all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
+
+router.get('/', (req, res) => {
+  res.status(200).send({
+    mensagem: 'OK',
+  });
+});
+
+router.post('/', (req, res) => {
 
 });
 
-router.post('/', (req, res, next) => {
-    
-})
+module.exports = router;
